cd91c824df81831c4f856274fc4a50078763f79e
dimentino <dimentej@gmail.com>
2024-05-31 16:12:32 +0300

    Looking for next/navigation possibilities

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+"use client";
+import React from "react";
+import {
+  useRouter,
+  useParams,
+  useSearchParams,
+  usePathname,
+} from "next/navigation";
 
 const PropertyPage = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const { id } = useParams();
+  const name = searchParams.get("name");
+  const pathName = usePathname();
   return (
-	<div>PropertyPage</div>
-  )
-}
+    <div>
+      <button onClick={() => router.push("/")} className="bg-blue-500 p-2">
+        Go Home {pathName}
+      </button>
+    </div>
+  );
+};
 
-export default PropertyPage
+export default PropertyPage;
