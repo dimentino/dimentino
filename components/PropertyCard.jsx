7642e7394217de852f857de89aedc33e151ac39c
dimentino <dimentej@gmail.com>
2024-06-02 17:32:46 +0300

    Added dynamic data to the PropertyCard

--- components/PropertyCard.jsx
+++ components/PropertyCard.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 import Image from "next/image";
-import { FaBed, FaBath, FaRulerCombined, FaMoneyBill, FaMapMarker } from 'react-icons/fa';
+import {
+  FaBed,
+  FaBath,
+  FaRulerCombined,
+  FaMoneyBill,
+  FaMapMarker,
+} from "react-icons/fa";
 import Link from "next/link";
 
-
-const PropertyCard = () => {
+const PropertyCard = ({ property }) => {
+  const getRateDisplay = () => {
+    const { rates } = property;
+    if (rates.monthly) {
+      return `${rates.monthly.toLocaleString()}/mo`;
+    } else if (rates.weekly) {
+      return `${rates.weekly.toLocaleString()}/wk`;
+    } else if (rates.nightly) {
+      return `${rates.nightly.toLocaleString()}/night`;
+    }
+  };
   return (
     <div className="rounded-xl shadow-md relative">
       <Image
-        src="/images/properties/a1.jpg"
+        src={`/images/properties/${property.images[0]}`}
         alt=""
         className="w-full h-auto rounded-t-xl"
         width={0}
@@ -17,44 +32,62 @@ const PropertyCard = () => {
       />
       <div className="p-4">
         <div className="text-left md:text-center lg:text-left mb-6">
-          <div className="text-gray-600">Apartment</div>
-          <h3 className="text-xl font-bold">Boston Commons Retreat</h3>
+          <div className="text-gray-600">{property.type}</div>
+          <h3 className="text-xl font-bold">{property.name}</h3>
         </div>
         <h3 className="absolute top-[10px] right-[10px] bg-white px-4 py-2 rounded-lg text-blue-500 font-bold text-right md:text-center lg:text-right">
-          {}
+          ${getRateDisplay()}
         </h3>
 
         <div className="flex justify-center gap-4 text-gray-500 mb-4">
           <p>
-            <FaBed className="inline mr-2" /> 3Beds
-            <span className="md:hidden lg:inline">Beds</span>
+            <FaBed className="inline mr-2" />
+            {property.beds}
+            {property.beds === 1 && (
+              <span className="md:hidden lg:inline"> Bed</span>
+            )}
+            {property.beds > 1 && (
+              <span className="md:hidden lg:inline"> Beds</span>
+            )}
           </p>
           <p>
-            <FaBath className="inline mr-2" /> 2Baths
-            <span className="md:hidden lg:inline">Baths</span>
+            <FaBath className="inline mr-2" />
+            {property.baths}
+            {property.baths === 1 && (
+              <span className="md:hidden lg:inline"> Bath</span>
+            )}
+            {property.baths > 1 && (
+              <span className="md:hidden lg:inline"> Baths</span>
+            )}
           </p>
           <p>
             <FaRulerCombined className="inline mr-2" />
-            1,500 sqft
-            <span className="md:hidden lg:inline">sqft</span>
+            {property.square_feet}
+            <span className="md:hidden lg:inline"> sqft</span>
           </p>
         </div>
 
         <div className="flex justify-center gap-4 text-green-900 text-sm mb-4">
+          {property.rates.nightly && (
             <p className="flex items-center">
               <FaMoneyBill className="inline mr-2" />
               Nightly
             </p>
+          )}
 
+          {property.rates.weekly && (
             <p className="flex items-center">
               <FaMoneyBill className="inline mr-2" />
               Weekly
             </p>
+          )}
 
+          {property.rates.monthly && (
             <p className="flex items-center">
               <FaMoneyBill className="inline mr-2" />
               Monthly
             </p>
+          )}
         </div>
 
         <div className="border border-gray-100 mb-5"></div>
@@ -63,11 +96,11 @@ const PropertyCard = () => {
           <div className="flex align-middle gap-2 mb-4 lg:mb-0">
             <FaMapMarker className="text-orange-700 mt-1" />
             <span className="text-orange-700">
-              Boston MA
+              {property.location.city} {property.location.state}
             </span>
           </div>
           <Link
-            href={`/properties`}
+            href={`/properties/${property._id}`}
             className="h-[36px] bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg text-center text-sm"
           >
             Details
