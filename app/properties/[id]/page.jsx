fbc4f214a32256bed48e451af00ab500b74cbd57
dimentino <dimentej@gmail.com>
2024-06-05 16:53:25 +0300

    Added Property Page layout and its dynamic data, added Spinner

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,24 +1,60 @@
 "use client";
-import React from "react";
-import {
-  useRouter,
-  useParams,
-  useSearchParams,
-  usePathname,
-} from "next/navigation";
+import { React, useEffect, useState } from "react";
+import { useParams } from "next/navigation";
+import { fetchProperty } from "@/utils/requests";
+import PropertyHeaderImage from "@/components/PropertyHeaderImage";
+import Link from "next/link";
+import { FaArrowLeft } from "react-icons/fa";
+import PropertyDetails from "@/components/PropertyDetails";
+import Spinner from "@/components/Spinner";
 
 const PropertyPage = () => {
-  const router = useRouter();
-  const searchParams = useSearchParams();
   const { id } = useParams();
-  const name = searchParams.get("name");
-  const pathName = usePathname();
+  const [property, setProperty] = useState(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchPropertyData = async () => {
+      if (!id) return;
+      try {
+        const property = await fetchProperty(id);
+        setProperty(property);
+      } catch (error) {
+        console.error("Error fetching property:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    if (property === null) {
+      fetchPropertyData();
+    }
+  }, [id, property]);
+  if (!property && !loading) {
+    return (
+      <h1 className="text-center text-2xl font-bold mt-10">
+        Property Not Found
+      </h1>
+    );
+  }
   return (
-    <div>
-      <button onClick={() => router.push("/")} className="bg-blue-500 p-2">
-        Go Home {pathName}
-      </button>
-    </div>
+    <>
+      {loading && <Spinner loading={loading} />}
+      {!loading && property && (
+        <>
+          <PropertyHeaderImage image={property.images[0]} />
+          <section>
+            <div className="container m-auto py-6 px-6">
+              <Link
+                href="/properties"
+                className="text-blue-500 hover:text-blue-600 flex items-center"
+              >
+                <FaArrowLeft className="mr-2"></FaArrowLeft> Back to Properties
+              </Link>
+            </div>
+          </section>
+          <PropertyDetails property={property} />
+        </>
+      )}
+    </>
   );
 };
 
